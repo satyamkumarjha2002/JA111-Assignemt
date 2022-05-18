cc69f1019aa56a9a44659a476f0ec2e60294cadd
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-05-18 23:41:33 +0530

    test 2

--- githubtest/src/com/masai/Github.java
+++ githubtest/src/com/masai/Github.java
@@ -4,7 +4,8 @@ public class Github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Github test");
+        System.out.println("Github test2");
 	}
 
 }
