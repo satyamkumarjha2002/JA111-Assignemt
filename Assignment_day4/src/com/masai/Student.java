dfe43cd9ec5b6d2a366ca028c3e6c1551c5fa408
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-05-22 13:24:52 +0530

    using constructure function question solved

--- Assignment_day4/src/com/masai/Student.java
+++ Assignment_day4/src/com/masai/Student.java
@@ -1,15 +1,44 @@
 package com.masai;
 
 public class Student {
-int roll;
-String name;
-String college;
-String address;
-public static Student getStudent(boolean isFromNIT)
-{
-return null;
-}
-static public void main(String[] args) {
-	
-}
+	int roll;
+	String name;
+	String college;
+	String address;
+	public static Student getStudent(boolean isFromNIT) {
+		if (isFromNIT == true) {
+			
+			return new Student(100,"Shivam","India");
+		} else {
+			return new Student(1,"Satyam","H.P.S","India");
+		}
+		
+	}
+	Student(int roll,String name,String college,String address){
+		this.name=name;
+		this.roll=roll;
+		this.college=college;
+		this.address=address;
+	}
+	Student(int roll,String name,String address){
+		this.name=name;
+		this.roll=roll;
+		this.college="NIT";
+		this.address=address;
+	}
+
+	static public void main(String[] args) {
+	  Student stu1=Student.getStudent(false);
+	  Student stu2=Student.getStudent(true);
+	  //using four parameterized constructor
+	  System.out.println(stu1.name);
+	  System.out.println(stu1.roll);
+	  System.out.println(stu1.college);
+	  System.out.println(stu1.address);
+	  //using three parameterized constructor
+	  System.out.println(stu2.name);
+	  System.out.println(stu2.roll);
+	  System.out.println(stu2.college);
+	  System.out.println(stu2.address);
+	}
 }
--- Assignment_day4/src/com/masai/Students.java
+++ Assignment_day4/src/com/masai/Students.java
@@ -1,20 +1,22 @@
 package com.masai;
 
 public class Students {
-	Students(){
-		
+	Students() {
+
 	}
-    Students(String name,int roll,int marks,int age){
-    	if(age>18&&age<60&&marks<500&&marks>0) {
-    		this.roll=roll;
-    		this.name=name;
-    		this.marks=marks;
-    		this.age=age;
-    	}
-    }
-int roll;
-String name;
-int age;
-int marks;
+
+	Students(String name, int roll, int marks, int age) {
+		if (age > 18 && age < 60 && marks < 500 && marks > 0) {
+			this.roll = roll;
+			this.name = name;
+			this.marks = marks;
+			this.age = age;
+		}
+	}
+
+	int roll;
+	String name;
+	int age;
+	int marks;
 
 }
