d2f9a21626a435b00f84571bca40349d4148e87b
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-05-22 10:38:20 +0530

    day1 assignment

---
+++ Day1 assignment/src/com/masai/Day1.java
@@ -0,0 +1,43 @@
+//Difference between JDK,JRE,JVM
+//1.Inside JDK all java development tools are available there;
+//2.Inside JRE predefined library and JVM is available;
+//3.JVM use for run the java program;
+
+//Write the difference between JSE, JEE and JME?
+//JSE(Java Standard Edition)-This is the core java platform here you can create database,GUI,multithreding type application;
+//JEE(Java Enterprise Edition)- This version of java is used in building big application;
+//JME(Java Micro Edition)- This version of java used in making small things like program of TV remote setupbox etc.
+
+
+//Q3) How does java achieve platform independence?
+//First java convert program in byte code and those byte you can run on any platform using JVM;
+
+
+//Q4) What are features of Java and explain them?
+//Java is very simple language easy to learn;
+//Java support object oriented concept almost all oops concept you can use in java;
+//Java is very secure language if you make a program in java those program are very secure;
+//APllication made by Java are very strong ;
+//Java has very good memory management system;
+
+
+
+
+
+
+package com.masai;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Name : Satyam Kumar Jha");
+        System.out.println("Father’s Name : Prem Chandra Jha");
+        System.out.println("Mother's Name : Madhavi Devi");
+        System.out.println("Age : 21");
+        System.out.println("Gender : Male");
+        System.out.println("Address : Vill-Bahera, Post-Bhith Bhagwanpur, Dist-Madhubani, State- Bihar,Pincode 847408");
+        System.out.println("");
+	}
+
+}
